71403af8afee5f4714c977eae4630301d456891d
jose reyes <codesmash50@gmail.com>
2016-09-20 22:43:27 -0400

    Added offcanvas nav and fixed header

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ $(function() {
 
   $(".hamburger").on("click", function(e) {
     console.log("I was clicked");
+
+    $('#drawer').toggleClass('open');
     e.preventDefault();
 
-    $(".menu").toggleClass('slide-down')
+    //$(".menu").toggleClass('slide-down')
   })
 
 });
